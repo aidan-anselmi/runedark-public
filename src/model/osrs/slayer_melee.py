338454e649f5abf1c2a4c4e3f322f965aea78d13
Aidan Anselmi <anselmi2011@gmail.com>
2026-01-11 12:34:49 -0800

    slayer melee

--- src/model/osrs/slayer_melee.py
+++ src/model/osrs/slayer_melee.py
@@ -155,7 +155,7 @@ class SlayerMelee(OSRSBot):
                     self.return_to_bank()
 
             # fight 
-            if self.check_idle_notifier_status("out_of_combat") or not self.has_no_hp_bar():
+            if self.check_idle_notifier_status("out_of_combat") or self.has_no_hp_bar():
                 self.atack_monster()
 
             # update progress
@@ -213,7 +213,7 @@ class SlayerMelee(OSRSBot):
         return True
     
     def has_no_hp_bar(self) -> bool:
-        for _ in range(5):
+        for _ in range(10):
             if self.has_hp_bar():
                 return False
             self.sleep(0.1)
