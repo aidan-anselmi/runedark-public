cdca7d6ed32f60fdd3161fe3cb22b7df3699439b
Aidan Anselmi <anselmi2011@gmail.com>
2026-01-15 14:19:08 -0800

    slayer melee

--- src/model/osrs/slayer_melee.py
+++ src/model/osrs/slayer_melee.py
@@ -284,7 +284,7 @@ class SlayerMelee(OSRSBot):
         if self.has_hp_bar():
             return False
 
-        if self.has_no_hp_bar_consec % 20 == 0:
+        if self.has_no_hp_bar_consec % 30 == 0:
             self.has_no_hp_bar_consec = 1
             return True
         else:
