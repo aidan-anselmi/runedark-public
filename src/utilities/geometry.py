0a8f963878592ad3e010c89bf752785cac9e16d8
Aidan Anselmi <anselmi2011@gmail.com>
2026-01-16 12:49:19 -0800

    slayer melee

--- src/utilities/geometry.py
+++ src/utilities/geometry.py
@@ -7,7 +7,7 @@ import numpy as np
 
 import utilities.random_util as rd
 
-Point = NamedTuple("Point", x=int, y=int)
+Point = NamedTuple("Point", x=int, y=int, z=int=0)
 
 # TO DO: Remove this global variable. This is a temporary fix for a bug in mss.
 sct = mss.mss()
