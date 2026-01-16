0b6d6d89954a6a9fe45f62e70831f8005534b805
Aidan Anselmi <anselmi2011@gmail.com>
2026-01-16 11:02:04 -0800

    slayer melee

--- src/utilities/mappings/colors_hsv.py
+++ src/utilities/mappings/colors_hsv.py
@@ -37,7 +37,7 @@ PURPLE = ((140, 255, 255), (140, 255, 255))
 PINK = ((165, 255, 255), (165, 255, 255))
 
 # Special colors
-GROUND_ITEM_COLOR = ((139, 230, 200), (141, 255, 255)) # purple with lower brightness
-GROUND_ITEM_TEXT_OUTLINE = ((0, 0, 0), (1, 1, 1))
+GROUND_ITEM_COLOR = ((138, 220, 190), (142, 255, 255)) # purple with lower brightness
+GROUND_ITEM_TEXT_OUTLINE = ((0, 0, 0), (2, 2, 2))
 
 # Colors selected from the Color Filter tool should be added below this line.
