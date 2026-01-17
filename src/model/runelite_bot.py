6daeef0adb5af163edf52798de2ebce15b2945d2
Aidan Anselmi <anselmi2011@gmail.com>
2026-01-16 23:39:19 -0800

    slayer melee

--- src/model/runelite_bot.py
+++ src/model/runelite_bot.py
@@ -2025,11 +2025,11 @@ class RuneLiteBot(Bot, metaclass=ABCMeta):
         # Take a screenshot of of the rectangle.
         char_screenshot_bgr = char_rect.screenshot()
         char_screenshot_hsv = cv2.cvtColor(char_screenshot_bgr, cv2.COLOR_BGR2HSV)
+        dbg.save_image("char_screenshot_hsv.png", char_screenshot_hsv)
         # Isolate HP bars in the rectangle.
         hp_bars = isolate_colors(
             char_screenshot_hsv, [self.cp.bgr.RED, self.cp.bgr.GREEN]
         )
-        dbg.save_image("hp_bars.png", hp_bars)
         # If there are any HP bars, return True.
         return hp_bars.mean(axis=(0, 1)) != 0.0
 
