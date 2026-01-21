aee492470e98334f9b8847073c9804ac1f0b9c95
Aidan Anselmi <anselmi2011@gmail.com>
2026-01-20 23:24:25 -0800

    abyss runecraft

--- src/model/osrs/abyss_rune_crafter.py
+++ src/model/osrs/abyss_rune_crafter.py
@@ -315,6 +315,7 @@ class AbyssRuneCrafter(OSRSBot):
         for _ in range(3):
             if click_altar():
                 break
+        self.sleep(lo=1, hi=2)
         self.click_pouches()
         for _ in range(3):
             if click_altar():
