e2bea63a17777e81962e039b85eb7d3c563d0a75
Aidan Anselmi <anselmi2011@gmail.com>
2026-01-20 17:33:31 -0800

    abyss runecraft

--- src/model/osrs/abyss_rune_crafter.py
+++ src/model/osrs/abyss_rune_crafter.py
@@ -250,7 +250,7 @@ class AbyssRuneCrafter(OSRSBot):
                     self.mouse.click()
                     self.sleep()
 
-        if rect := self.find_sprite(win=self.win.game_view, png="pure-essence.png", folder="items"):
+        if rect := self.find_sprite(win=self.win.game_view, png="pure-essence.png", folder="items", confidence=0.05):
             self.mouse.move_to(rect.random_point())
             self.mouse.click()
             self.sleep()
@@ -259,7 +259,7 @@ class AbyssRuneCrafter(OSRSBot):
                 self.mouse.move_to(rect.random_point())
                 self.mouse.click()
                 self.sleep()
-        if rect := self.find_sprite(win=self.win.game_view, png="pure-essence.png", folder="items"):
+        if rect := self.find_sprite(win=self.win.game_view, png="pure-essence.png", folder="items", confidence=0.05):
             self.mouse.move_to(rect.random_point())
             self.mouse.click()
             self.sleep()
